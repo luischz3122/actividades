f10f344cd91215c26dd1d3282f2359da82f9900f
luischz3122 <lsanchez@codevschool.org>
2020-02-20 04:43:11 +0000

    Cree una carpeta titulada Actividad 02, donde se encuentran 6 actividades basicas en python

--- alumnos/Luis Omar/Actividad 02/Actividad-6.py
+++ alumnos/Luis Omar/Actividad 02/Actividad-6.py
@@ -1,28 +1,3 @@
-#act.1
-print ("Hola Mundo ")
-
-
-#act.2
-saludo="Hola mi nombre es Luis"
-#act.3
-x=5
-y=6
-print(x+y)
-
-
-#act.4
-a=18
-b=2
-suma=a+b
-print(suma)
-
-
-#act.5
-print("Hola,¿Cual es tu nombre?")
-nombre=str(input("Ingresar nombre:"))
-print(str(nombre)+",mucho gusto.")
-
-
 #act.6
 print("Programa que calcula el area de un triangulo\n")
 
