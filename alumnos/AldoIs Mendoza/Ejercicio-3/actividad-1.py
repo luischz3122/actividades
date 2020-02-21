40e7179b8b8fb925545bce7d0b3e81751b07660d
Aldo Ismael Mendoza Betancourt <aldois.rov@gmail.com>
2020-02-21 04:46:11 +0000

    Actividad 3

---
+++ alumnos/AldoIs Mendoza/Ejercicio-3/actividad-1.py
@@ -0,0 +1,6 @@
+estaLloviendo = "si"
+while(estaLloviendo != "no"): 
+	estaLloviendo = input("¿Esta lloviendo en este momento \n?")
+	if(estaLloviendo != "no"):
+		print("Hay que usar el paraguas")
+print("Que bueno que ya dejo de llover")
