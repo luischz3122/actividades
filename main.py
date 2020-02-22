775ba9838d1ffd8836b36203cc5c7880432d958e
jarquisol <jsandoval@codevschool.org>
2020-02-22 04:56:47 +0000

    Agregué Actividad 04

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+calificacionesAlumno = [8, 7, 9, 10, 7, 7, 7]
 
+calificacionTotal = 0
+numeroParciales = len(calificacionesAlumno)
+for calificacionParcial in calificacionesAlumno:
+    calificacionTotal = calificacionTotal + calificacionParcial
+
+p = calificacionTotal / numeroParciales
+print("El promedio fue: " + str(p) )
